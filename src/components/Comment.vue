86926ec05c0ebe08079bab75d7ab3c5dba9c7877
Michael Jolley <mike@sparcapp.io>
2020-05-27 16:26:47 -0500

    Working on comments

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -12,7 +12,7 @@
       <cite>
         <span class="author">{{ comment.name }}</span>
         <span class="bullet"> • </span>
-        <a :href="'#' + comment.id" class="muted" :title="comment.date">{{ comment.date }}</a>
+        <a :href="'#' + comment.id" :title="comment.date">{{ comment.date }}</a>
       </cite>
       <div class="comment-body">{{ comment.message }}</div>
     </blockquote>
@@ -23,3 +23,42 @@ export default {
   props: ["comment"]
 };
 </script>
+<style lang="scss">
+li {
+  padding: 20px;
+  padding-bottom: 0;
+  display: flex;
+  font-size: .9em;
+
+  blockquote {
+    margin-top: 0;
+    color: $white;
+
+    cite {
+      .author {
+        color: $pink;
+        font-weight: bold;
+      }
+
+      a {
+        color: $gray;
+        font-style: normal;
+        font-size: .9em;
+        &:hover {
+          color: $pink;
+        }
+      }
+    }
+
+    div.comment-body {
+      line-height: 21px;
+    }
+  }
+
+  .byauthor {
+    border-radius: 4px;
+    background-color: #fcf9eb;
+  }
+
+}
+</style>
--- src/components/layout/Social.vue
+++ src/components/layout/Social.vue
@@ -71,7 +71,6 @@ export default {};
 
     svg {
       padding-top: 8px;
-     
     }
 
     &.twitter:hover {
