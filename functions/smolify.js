2125b8d6bafcd5cca17600461cbc2334be578a53
Michael Jolley <mike@sparcapp.io>
2020-06-16 23:31:35 -0500

    Fixing issues with smolify function

--- functions/smolify.js
+++ functions/smolify.js
@@ -1,67 +1,67 @@
-const Lokka = require('lokka').Lokka;
-const Transport = require('lokka-transport-http').Transport;
+const Lokka = require("lokka").Lokka;
+const Transport = require("lokka-transport-http").Transport;
 
-const faunaSecret = process.env.FAUNADBSECRET;
+const faunaSecret = "fnADugDC4HACE30TjhyAct0bFNhchsZh6aR7oMN6"; // process.env.FAUNADBSECRET;
 
 const headers = {
-  Authorization: `Bearer ${faunaSecret}`
+  Authorization: `Bearer ${faunaSecret}`,
 };
-const transport = new Transport('https://graphql.fauna.com/graphql', { headers });
+const transport = new Transport("https://graphql.fauna.com/graphql", {
+  headers,
+});
 
 const client = new Lokka({
-  transport: transport
+  transport: transport,
 });
 
 exports.handler = async (event, context) => {
+  let redirectUrl = "https://baldbeardedbuilder.com/";
+  const path = event.queryStringParameters.path;
 
-  let redirectUrl = 'https://baldbeardedbuilder.com/'
+  console.dir(event);
 
-  const path = event.queryStringParameters.path
+  const realdeal = await getLongUrl(path);
 
-  const realdeal = await getLongUrl(path)
-
-  if (realdeal) {
-    redirectUrl = realdeal.url
+  if (realdeal && realdeal.url) {
+    redirectUrl = realdeal.url;
   }
- 
+
   return {
-      statusCode: 302,
-      headers: {
-        location: redirectUrl,
-        'Cache-Control': 'no-cache',
-      },
-      body: JSON.stringify({ })
-    }
-}
+    statusCode: 302,
+    headers: {
+      location: redirectUrl,
+      "Cache-Control": "no-cache",
+    },
+    body: JSON.stringify({}),
+  };
+};
 
 const getLongUrl = async (path) => {
-
-  let longUrl = undefined
+  let longUrl = undefined;
 
   // Lookup path in FaunaDb & get the longUrl if it exists
   try {
     const variables = {
-      source: path
-    }
+      source: path,
+    };
 
-    const data = await client.query(query, variables)
+    const data = await client.query(query, variables);
 
-    if (data.shortyMcShortLinkBySource && data.shortyMcShortLinkBySource.data) {
-
-      const shortLink = data.shortyMcShortLinkBySource.data
+    if (data.shortyMcShortLinkBySource && data.shortyMcShortLinkBySource) {
+      const shortLink = data.shortyMcShortLinkBySource;
 
       // TODO: Record the visit
 
-      longUrl = shortLink.target
+      longUrl = shortLink.target;
     }
   } catch (err) {
     console.log(err);
   }
 
   return {
-    url: longUrl
-  }
-}
+    url: longUrl,
+  };
+};
 
 const query = `
   query getEm($source: String!) {
@@ -71,4 +71,4 @@ const query = `
       visits
     }
   }
-`
+`;
