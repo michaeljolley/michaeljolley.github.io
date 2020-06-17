ef1608c7029523fc281d4ce55bb2d7ec3a363785
Michael Jolley <mike@sparcapp.io>
2020-06-16 23:53:04 -0500

    Fixing secrets

--- functions/smolify.js
+++ functions/smolify.js
@@ -1,7 +1,7 @@
 const Lokka = require("lokka").Lokka;
 const Transport = require("lokka-transport-http").Transport;
 
-const faunaSecret = "fnADugDC4HACE30TjhyAct0bFNhchsZh6aR7oMN6"; // process.env.FAUNADBSECRET;
+const faunaSecret = process.env.FAUNADBSECRET;
 
 const headers = {
   Authorization: `Bearer ${faunaSecret}`,
