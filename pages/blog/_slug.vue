3cf10825209af06acbcc6a3ce2470cfc45fd3601
Michael Jolley <mike@sparcapp.io>
2020-12-08 14:50:51 -0600

    We done doing the do

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,25 +1,33 @@
 <template>
-	<article>
-		<header>
-			<cld-image
-				v-if="post.cover && post.cover.public_id"
-				:alt="post.title"
-				:public-id="post.cover.public_id"
-				quality="auto"
-				fetch-format="auto"
-				responsive
-				loading="lazy"
-			/>
-		</header>
-		<section>
-			<nuxt-content :document="post" />
-		</section>
-		<section>
-			<p v-for="comment in comments" :key="comment.slug">
-				{{ comment.message }}
-			</p>
-		</section>
-	</article>
+	<div class="post">
+		<div class="toc">
+			<ul>
+				<li
+					v-for="link of post.toc"
+					:key="link.id"
+					:class="{ toc2: link.depth === 2, toc3: link.depth === 3 }"
+				>
+					<NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+				</li>
+			</ul>
+		</div>
+		<article>
+			<header>
+				<cld-image
+					v-if="post.cover && post.cover.public_id"
+					:alt="post.title"
+					:public-id="post.cover.public_id"
+					quality="auto"
+					fetch-format="auto"
+					responsive
+					loading="lazy"
+				/>
+			</header>
+			<section>
+				<nuxt-content :document="post" />
+			</section>
+		</article>
+	</div>
 </template>
 
 <script>
@@ -27,13 +35,8 @@ export default {
 	async asyncData({ $content, params }) {
 		const post = await $content('/blog', params.slug).fetch()
 
-		const comments = await $content('/comments')
-			.where({ postslug: { $eq: params.slug } })
-			.fetch()
-
 		return {
 			post,
-			comments,
 		}
 	},
 	head() {
@@ -69,4 +72,32 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.post {
+	@apply flex;
+	/* @apply space-x-4; */
+	@apply mt-10;
+}
+
+.toc {
+	@apply flex;
+	@apply flex-col;
+	@apply w-1/5;
+	@apply bg-gray-500;
+	@apply rounded-xl;
+	@apply mr-10;
+	@apply p-3;
+}
+
+article {
+	@apply flex;
+	@apply flex-col;
+	@apply w-4/5;
+}
+
+section {
+	@apply p-10;
+	@apply bg-gray-500;
+	@apply rounded-b-xl;
+}
+</style>
