6d251c27c510a353c05e1fa03e1fe1faaf7b2433
Michael Jolley <mike@sparcapp.io>
2020-12-11 22:35:23 -0600

    Removing bad files

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Bald. Bearded. Builder.",
-  "siteUrl": "https://baldbeardedbuilder.com",
-  "version": "0.7.12"
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1596433893271
